2790a8597731ccd885cd03250bb5183261633204
Nanoteck137 <patrik.millvik@gmail.com>
2018-10-23 09:36:34 +0200

    Created the tables

--- src/net/nanoteck137/physics/Program.java
+++ src/net/nanoteck137/physics/Program.java
@@ -1,8 +1,10 @@
 package net.nanoteck137.physics;
 
+import net.nanoteck137.physics.util.Constants;
+
 public class Program {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(Constants.c);
     }
 }
